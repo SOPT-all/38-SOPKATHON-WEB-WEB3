6432ad75ecc8e260929d486f8e6b05f6deb78586
jjangminii <wjdals030328@catholic.ac.kr>
2026-05-17 04:28:26 +0900

    init: svgr 세팅

---
+++ src/shared/assets/svg/IcEdit.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const SvgIcEdit = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M18.725 5.782a2.664 2.664 0 0 1 3.77-.003l3.324 3.324a2.67 2.67 0 0 1 .022 3.75l-2.212 2.27-7.12-7.121zm-3.63 3.636 7.137 7.137-8.7 8.927a4 4 0 0 1-2.864 1.209h-3.67a2 2 0 0 1-1.996-2.084l.157-3.746a4 4 0 0 1 1.168-2.662zm3.097 17.175a1 1 0 0 0 1 1h8.161a1 1 0 0 0 0-2h-8.162a1 1 0 0 0-1 1"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgIcEdit;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        jsxRuntime: 'automatic',
+      },
+    }),
+  ],
 });
