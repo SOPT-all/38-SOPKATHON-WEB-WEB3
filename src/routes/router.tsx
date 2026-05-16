7bc7a11209fe9369847d61bf7bfa22942f94f8a8
jjangminii <wjdals030328@catholic.ac.kr>
2026-05-17 01:02:14 +0900

    fix: lint error 해결

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import { routePath } from '@routes/path';
 
 import DetailPage from '@pages/detailPage/DetailPage';
 import Home from '@pages/home/Home';
 import MyPage from '@pages/myPage/MyPage';
 import WritePage from '@pages/writePage/WritePage';
+
+import { routePath } from '@routes/path';
 
 export const router = createBrowserRouter([
   {
