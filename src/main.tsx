25230f33bf7b9cc2324da562b60ce01ceb95e8fb
yurimidaH <hyr4249319@gmail.com>
2026-05-16 11:29:55 +0900

    init: tanstack query 및 axios 초기세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@routes/router';
 
+import { queryClient } from '@apis/query-client';
+
 import './shared/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
